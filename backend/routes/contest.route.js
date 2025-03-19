6ee4c3fe7476664bf39eec7d130937d3b3b0ea59
stalin670 <amitalvin01@gmail.com>
2025-03-19 21:41:10 +0530

    created and setup api routes

--- backend/routes/contest.route.js
+++ backend/routes/contest.route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { codeforces, getSolution, addSolution } = require("../controllers/contest.controller.js");
+const { codeforces, getSolution, addSolution, leetcode, codechef, atcoder } = require("../controllers/contest.controller.js");
 
 router.get("/codeforces", codeforces);
+router.get("/leetcode", leetcode);
+router.get("/codechef", codechef);
+router.get("/atcoder", atcoder);
 
 router.post("/add-solution", addSolution);
 router.get("/solution/:contestId", getSolution);
