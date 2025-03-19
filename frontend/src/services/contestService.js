ee65c3a7be1fa3368daa94030ec06a550a34b0d9
stalin670 <amitalvin01@gmail.com>
2025-03-19 20:53:39 +0530

    replace files

--- frontend/src/services/contestService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const getCodeforcesContests = async () => {
-    const response = await axios.get('http://localhost:8000/api/contests/codeforces');
-    return response.data;
-};
