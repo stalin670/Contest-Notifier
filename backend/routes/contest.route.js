1f26a2419cf7dcc883b15126a5c5018a831e371b
stalin670 <amitalvin01@gmail.com>
2025-03-19 20:29:39 +0530

    created addSolution and getSolution route

--- backend/routes/contest.route.js
+++ backend/routes/contest.route.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { codeforces } = require("../controllers/contest.controller.js");
+const { codeforces, getSolution, addSolution } = require("../controllers/contest.controller.js");
 
 router.get("/codeforces", codeforces);
+
+router.post("/add-solution", addSolution);
+router.get("/solution/:contestId", getSolution);
 
 module.exports = router;
