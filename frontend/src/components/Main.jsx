854ef5eac6047f439bfd9dbce88b124e7033354f
stalin670 <amitalvin01@gmail.com>
2025-03-20 16:21:23 +0530

    added in Main page colors

--- frontend/src/components/Main.jsx
+++ frontend/src/components/Main.jsx
@@ -98,12 +98,12 @@ const Main = () => {
     };
 
     return (
-        <div className="">
+        <div className="dark:bg-black pb-5">
             <div className="text-white text-center p-4 mx-auto">
                 {["codeforces", "leetcode", "atcoder", "codechef"].map(platform => (
                     <button
                         key={platform}
-                        className={`mx-1 btn ${visibleContests?.includes(platform) ? 'bg-blue-600 text-white' : 'btn-outline border-gray-300 text-gray-500'} hover:bg-gray-200 rounded-full`}
+                        className={`mx-1 btn ${visibleContests?.includes(platform) ? 'bg-blue-500 text-white dark:border-grey dark:border-2 dark:border-solid' : 'btn-outline border-gray-300 text-gray-500 dark:text-white dark:border-grey dark:border-2 dark:border-solid'} hover:bg-gray-200 dark:hover:text-blue-500 rounded-full`}
                         onClick={() => handleClick(platform)}
                     >
                         {platform.charAt(0).toUpperCase() + platform.slice(1)}
@@ -113,27 +113,34 @@ const Main = () => {
 
             <div className="tabs tabs-lifted sm:mx-5 md:mx-10 lg:mx-10 p-2 ">
                 <button
-                    className={`tab mx-2 py-2 px-4 font-bold rounded-full ${activeTab === 1 ? 'bg-gray-300 text-white ' : 'hover:bg-gray-300'
-                        }`}
+                    className={`tab mx-2 py-2 px-4 font-bold rounded-full 
+            ${activeTab === 1 ?
+                            '!bg-gray-300 !border-2 !border-black dark:!bg-blue-500 dark:!text-white dark:!border-white'
+                            : 'dark:!bg-black dark:!border-white dark:!text-white dark:hover:!text-blue-500 dark:hover:!bg-gray-200 border-2 border-black'}`}
                     onClick={() => setActiveTab(1)}
                 >
                     Upcoming
                 </button>
                 <button
-                    className={`tab mx-2 py-2 px-4 font-bold rounded-full ${activeTab === 2 ? 'bg-gray-300 text-white' : 'hover:bg-gray-300'
-                        }`}
+                    className={`tab mx-2 py-2 px-4 font-bold rounded-full 
+            ${activeTab === 2 ?
+                            '!bg-gray-300 !text-white dark:!bg-blue-500 dark:!text-white'
+                            : 'dark:!bg-black dark:!border-white dark:!text-white hover:!bg-gray-300 !border-2 !border-black'}`}
                     onClick={() => setActiveTab(2)}
                 >
                     Recent
                 </button>
                 <button
-                    className={`tab mx-2 py-2 px-4 font-bold rounded-full ${activeTab === 3 ? 'bg-gray-300 text-white' : 'hover:bg-gray-300'
-                        }`}
+                    className={`tab mx-2 py-2 px-4 font-bold rounded-full 
+            ${activeTab === 3 ?
+                            '!bg-gray-300 !text-white dark:!bg-blue-500 dark:!text-white'
+                            : 'dark:!bg-black dark:!border-white dark:!text-white hover:!bg-gray-300 !border-2 !border-black'}`}
                     onClick={() => setActiveTab(3)}
                 >
                     Bookmarked
                 </button>
             </div>
+
 
             <div className="">
                 {activeTab === 1 && (
