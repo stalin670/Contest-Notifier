38d2a763d119760a1736ad62a79048b1243e69fc
stalin670 <amitalvin01@gmail.com>
2025-03-20 18:05:45 +0530

    update cors origin

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,10 @@ const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 8001;
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
